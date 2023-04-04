d86da0e92d90e95284b331a50c21c81557a287c4
Andrey Savelev <a.v.savelev@foxford.ru>
2023-04-04 23:40:16 +0400

    variables in separated script

--- simpl_raycast.py
+++ simpl_raycast.py
@@ -2,47 +2,9 @@ import pygame
 import sys
 import math
 import simple_raycast.color as my
+from simple_raycast.variables import *
 
 pygame.init()
-
-forward = True
-draw2D = True
-
-SCREEN_HEIGHT = 480
-
-MULT_SCREEN_WIDTH = 2 if draw2D else 1
-SHIFT_WIDTH = SCREEN_HEIGHT if draw2D else 0
-
-SCREEN_WIDTH = SCREEN_HEIGHT * MULT_SCREEN_WIDTH
-MAP_SIZE = 8
-TILE_SIZE = ((SCREEN_WIDTH / 2 ) / MAP_SIZE)
-MAX_DEPTH = int(MAP_SIZE * TILE_SIZE)
-FOV = math.pi / 3
-HALF_FOV = FOV / 2
-CASTED_RAYS = 120
-STEP_ANGLE = FOV / CASTED_RAYS
-SCALE = (SCREEN_WIDTH / 2) / CASTED_RAYS
-
-player_x = (SCREEN_WIDTH / 2) / 2
-player_y = (SCREEN_WIDTH / 2) / 2
-player_angle = math.pi
-
-wallID = "#"
-
-win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Raycasting by Network Skeleton")
-clock = pygame.time.Clock()
-
-MAP = (
-    '########'
-    '#   ## #'
-    '#      #'
-    '#    ###'
-    '##     #'
-    '#   #  #'
-    '#   #  #'
-    '########'
-)
 
 def draw_2D_cell(col,row):
     index = row * MAP_SIZE + col
