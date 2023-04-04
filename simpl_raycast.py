e92c9cb039395f90532a77f4cd02083db3a20e04
Andrey Savelev <a.v.savelev@foxford.ru>
2023-04-05 00:01:23 +0400

    render3D in separate script

--- simpl_raycast.py
+++ simpl_raycast.py
@@ -2,28 +2,13 @@ import pygame
 import math
 import sys
 import simple_raycast.render2D as render2D
+import simple_raycast.render3D as render3D
 import simple_raycast.variables as _var_
 import simple_raycast.collision as _collision_
 import simple_raycast.input as _input_
 
 pygame.init()
 clock = pygame.time.Clock()
-
-def draw_3D_back():
-    pygame.draw.rect(_var_.win, (100, 0, 0),
-                     (_var_.SHIFT_WIDTH, _var_.SCREEN_HEIGHT / 2, _var_.SCREEN_HEIGHT, _var_.SCREEN_HEIGHT))
-    pygame.draw.rect(_var_.win, (200, 0, 0),
-                     (_var_.SHIFT_WIDTH, -_var_.SCREEN_HEIGHT / 2, _var_.SCREEN_HEIGHT, _var_.SCREEN_HEIGHT))
-
-
-def draw_3D_wall_segment(ray, depth, start_angle):
-    color = 50 / (1 + depth * depth * 0.0001)
-    depth *= math.cos(_var_.player_angle - start_angle)
-    wall_height = 21000 / (depth + 0.0001)
-    if wall_height > _var_.SCREEN_HEIGHT: wall_height == _var_.SCREEN_HEIGHT
-
-    pygame.draw.rect(_var_.win, (color, color, color), (
-        _var_.SCREEN_HEIGHT + ray * _var_.SCALE, (_var_.SCREEN_HEIGHT / 2) - wall_height / 2, _var_.SCALE, wall_height))
 
 
 def cast_rays(player_x, player_y):
@@ -39,7 +24,7 @@ def cast_rays(player_x, player_y):
 
             if _var_.MAP[index] == _var_.wallID:
                 render2D.draw_2D_rays(col, row, target_x, target_y, player_x, player_y)
-                draw_3D_wall_segment(ray, depth, start_angle)
+                render3D.draw_3D_wall_segment(ray, depth, start_angle)
                 break
 
         start_angle += _var_.STEP_ANGLE
@@ -51,15 +36,16 @@ while True:
             pygame.quit()
             sys.exit(0)
 
-    player_x,player_y,player_angle = _var_.player_x,_var_.player_y,_var_.player_angle
-
+    player_x, player_y, player_angle = _var_.player_x, _var_.player_y, _var_.player_angle
     forward = _var_.forward
-    player_x, player_y = _collision_.check_collision(player_x, player_y, player_angle,forward)
+
+    player_x, player_y = _collision_.check_collision(player_x, player_y, player_angle, forward)
     render2D.draw_2D_map(player_x, player_y, player_angle)
-    draw_3D_back()
+    render3D.draw_3D_back()
     cast_rays(player_x, player_y)
 
-    _var_.player_angle, _var_.player_x, _var_.player_y, _var_.forward = _input_.input_scan(player_angle, player_x, player_y,forward)
+    _var_.player_angle, _var_.player_x, _var_.player_y, _var_.forward = _input_.input_scan(player_angle, player_x,
+                                                                                           player_y, forward)
 
     clock.tick(60)
     fps = str(int(clock.get_fps()))
---
+++ simple_raycast/render3D.py
@@ -0,0 +1,19 @@
+import pygame
+import math
+import simple_raycast.variables as _var_
+
+def draw_3D_back():
+    pygame.draw.rect(_var_.win, (100, 0, 0),
+                     (_var_.SHIFT_WIDTH, _var_.SCREEN_HEIGHT / 2, _var_.SCREEN_HEIGHT, _var_.SCREEN_HEIGHT))
+    pygame.draw.rect(_var_.win, (200, 0, 0),
+                     (_var_.SHIFT_WIDTH, -_var_.SCREEN_HEIGHT / 2, _var_.SCREEN_HEIGHT, _var_.SCREEN_HEIGHT))
+
+
+def draw_3D_wall_segment(ray, depth, start_angle):
+    color = 50 / (1 + depth * depth * 0.0001)
+    depth *= math.cos(_var_.player_angle - start_angle)
+    wall_height = 21000 / (depth + 0.0001)
+    if wall_height > _var_.SCREEN_HEIGHT: wall_height == _var_.SCREEN_HEIGHT
+
+    pygame.draw.rect(_var_.win, (color, color, color), (
+        _var_.SCREEN_HEIGHT + ray * _var_.SCALE, (_var_.SCREEN_HEIGHT / 2) - wall_height / 2, _var_.SCALE, wall_height))
