3c08718042ffa6286dab4d5f79ead878e6ab2159
Andrey Savelev <a.v.savelev@foxford.ru>
2023-04-05 00:07:05 +0400

    Update simpl_raycast.py

--- simpl_raycast.py
+++ simpl_raycast.py
@@ -16,17 +16,13 @@ while True:
             pygame.quit()
             sys.exit(0)
 
-    player_x, player_y, player_angle = _var_.player_x, _var_.player_y, _var_.player_angle
-    forward = _var_.forward
-
-    player_x, player_y = _collision_.check_collision(player_x, player_y, player_angle, forward)
-    render2D.draw_2D_map(player_x, player_y, player_angle)
+    fixed_x, fixed_y = _collision_.check_collision(_var_.player_x, _var_.player_y, _var_.player_angle, _var_.forward)
+    render2D.draw_2D_map(fixed_x, fixed_y, _var_.player_angle)
     render3D.draw_3D_back()
-    _raycast_.cast_rays(player_x, player_y)
+    _raycast_.cast_rays(fixed_x, fixed_y)
 
-    _var_.player_angle, _var_.player_x, _var_.player_y, _var_.forward = _input_.input_scan(player_angle, player_x,
-                                                                                           player_y, forward)
-
+    _var_.player_angle, _var_.player_x, _var_.player_y, _var_.forward = _input_.input_scan(_var_.player_angle, fixed_x,
+                                                                                           fixed_y, _var_.forward)
     clock.tick(60)
     fps = str(int(clock.get_fps()))
     font = pygame.font.SysFont('Monospace Regular', 30)
