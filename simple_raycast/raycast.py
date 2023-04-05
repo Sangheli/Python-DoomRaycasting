6f2209bbf960ec8c1e219d737a11283b1446c0ea
Andrey Savelev <a.v.savelev@foxford.ru>
2023-04-06 01:04:52 +0400

    raycasting optimization

--- simple_raycast/raycast.py
+++ simple_raycast/raycast.py
@@ -6,9 +6,9 @@ import simple_raycast.map as map
 
 
 # Получаем проекции треугольника(x,y), исходя из угла и длины гипотенузы(angle,depth)
-def get_ray_projection(player_x, player_y, angle, depth):
-    ray_x = player_x - math.sin(angle) * depth
-    ray_y = player_y + math.cos(angle) * depth
+def get_ray_projection(player_x, player_y, _sin,_cos, depth):
+    ray_x = player_x - _sin * depth
+    ray_y = player_y + _cos * depth
     return ray_x, ray_y
 
 
@@ -17,9 +17,11 @@ def cast_rays(player_x, player_y):
 
     for ray_index in range(_var_.CASTED_RAYS):
         angle = start_angle + ray_index * _var_.STEP_ANGLE
+        _sin = math.sin(angle)
+        _cos = math.cos(angle)
         # Проверяем луч на каждом из указанных шагов глубины
         for depth in range(0,_var_.MAX_DEPTH,2):
-            ray_x, ray_y = get_ray_projection(player_x, player_y, angle, depth)
+            ray_x, ray_y = get_ray_projection(player_x, player_y,_sin,_cos, depth)
             col, row = map.get_coordinates(ray_x, ray_y)
             if map.is_wall(col, row):
                 render2D.draw_2D_rays(col, row, ray_x, ray_y, player_x, player_y)
