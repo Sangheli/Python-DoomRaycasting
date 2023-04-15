88fd594a4d924d0f8bbf2c845766c038397f62e7
Andrey Savelev <a.v.savelev@foxford.ru>
2023-04-15 16:53:15 +0400

    speedup raycast func

--- simple_raycast/raycast.py
+++ simple_raycast/raycast.py
@@ -34,3 +34,37 @@ def cast_rays(player_x, player_y):
                 render2D.draw_2D_rays(col, row, ray_x, ray_y, player_x, player_y)
                 render3D.draw_3D_wall_segment(ray_index, depth, angle)
                 break
+
+def cast_rays_new(player_x, player_y):
+    count = 0
+    depth_step = 10
+
+    start_angle = _var_.player_angle - _var_.HALF_FOV
+
+    for ray_index in range(_var_.CASTED_RAYS):
+        big_count = 0
+        angle = start_angle + ray_index * _var_.STEP_ANGLE
+        _sin = math.sin(angle)
+        _cos = math.cos(angle)
+        # Проверяем луч на каждом из указанных шагов глубины
+        for depth in range(0,_var_.MAX_DEPTH,depth_step):
+            big_count+=1
+            ray_x, ray_y = get_ray_projection(player_x, player_y,_sin,_cos, depth)
+            col, row = map.get_coordinates(ray_x, ray_y)
+            if map.is_wall(col, row):
+                count += big_count - 1 + cast_subrays(player_x, player_y, _sin, _cos, ray_index, angle, depth - depth_step)
+                break
+
+    print_raycount(count)
+
+def cast_subrays(player_x, player_y, _sin, _cos, ray_index, angle, start):
+    subCount = 0
+    for depth in range(start, _var_.MAX_DEPTH,1):
+        subCount+=1
+        ray_x, ray_y = get_ray_projection(player_x, player_y, _sin, _cos, depth)
+        col, row = map.get_coordinates(ray_x, ray_y)
+        # Проверяем луч на каждом из указанных шагов глубины
+        if map.is_wall(col, row):
+            render2D.draw_2D_rays(col, row, ray_x, ray_y, player_x, player_y)
+            render3D.draw_3D_wall_segment(ray_index, depth, angle)
+            return subCount
